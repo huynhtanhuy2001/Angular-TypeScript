ca6d74984c0ef6d128826b0e2ad0286c9ddd4f42
huynhtanhuy2001 <huyhuynh47471@gmail.com>
2022-12-05 20:32:38 +0700

    Lab2

---
+++ Lab_2/Bai_1/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // templateUrl: './app.component.html',
+  template:'<app-user-info></app-user-info>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bai1';
+
+ 
+}
---
+++ Lab_2/Bai_1/app/user-info/user-info.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info',
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.css'],
+})
+export class UserInfoComponent {
+  username: string = '';
+  constructor() {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+  onReset() {
+    this.username = '';
+  }
+}
